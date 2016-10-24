c917bbf564073f00cc33321bdd19e9dff886fe71
Nick P <nicolas.papanicolaou20@gmail.com>
2016-10-23 22:42:23 -0400

    Bonhomme

---
+++ TP1/Assets/Script/Hero.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public GameObject character;
+	SpriteRenderer hero;
+	public Sprite frontStand;
+	public Sprite frontLeftArm;
+	public Sprite frontRightArm;
+
+	public Sprite leftStand;
+	public Sprite leftLeftArm;
+	public Sprite leftRightArm;
+
+	public Sprite backStand;
+	public Sprite backLeftArm;
+	public Sprite backRightArm;
+
+	public Sprite rightStand;
+	public Sprite rightLeftArm;
+	public Sprite rightRightArm;
+	static int count = 0;
+
+
+	void Start()
+	{
+		
+		hero = character.GetComponent<SpriteRenderer>();
+		hero.sprite = frontStand;
+	}
+
+	void LeftArrow(){
+		
+		if (hero.sprite == leftRightArm) {
+			Debug.Log ("test");
+			count++;
+			hero.sprite = leftStand;
+		} else if (hero.sprite == leftLeftArm) {
+			count--;
+			hero.sprite = leftStand;
+		} else {
+			if (count == 0) {
+				hero.sprite = leftRightArm;
+			} else {
+				hero.sprite = leftLeftArm;
+			}
+		}
+	}
+
+
+	void Update() {
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			Debug.Log ("test");
+			LeftArrow ();
+		}
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+				character.transform.position += move * speed * Time.deltaTime;
+	}
+
+
+
+}
---
+++ TP1/Assets/Script/HeroMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMovement : MonoBehaviour {
+	private Rigidbody2D rigi;
+	public float maxSpeed = 10f;
+	bool facingFront = true;
+	bool facingLeft = false;
+	bool facingRight = false;
+	bool facingBack = false;
+
+	Animator animator;
+
+	void Start(){
+		rigi = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update(){
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			animator.SetBool ("Down", true);
+			transform.position += move * maxSpeed * Time.deltaTime;
+
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			animator.SetBool ("Up", true);
+			transform.position += move * maxSpeed * Time.deltaTime;
+
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			animator.SetBool ("Left", true);
+			transform.position += move * maxSpeed * Time.deltaTime;
+
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			animator.SetBool ("Right", true);
+			transform.position += move * maxSpeed * Time.deltaTime;
+
+		} else {
+			animator.SetBool ("Down", false);
+			animator.SetBool ("Up", false);
+			animator.SetBool ("Left", false);
+			animator.SetBool ("Right", false);
+
+		}
+
+
+	}
+
+
+		
+
+}
+
