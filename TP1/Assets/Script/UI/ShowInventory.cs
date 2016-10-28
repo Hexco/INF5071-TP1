62ec74fdba7b3c95ccc51496184d053e0fd9a56f
francis <bilodeau-robilalrd.francis@courrier.uqam.ca>
2016-10-28 16:34:26 -0400

    Inventaire de base (i pour fermer et ouvrir)

---
+++ TP1/Assets/Script/UI/ShowInventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowInventory : MonoBehaviour
+{
+
+    public GameObject inventaire; // Assign in inspector
+    private bool isActive;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            isActive = !isActive;
+            inventaire.SetActive(isActive);
+        }
+    }
+
+    public void resumeGame()
+    {
+        isActive = !isActive;
+        inventaire.SetActive(isActive);
+    }
+}
