5ae6e0745406876b5be21fe111219ffaf44c4ea7
hexco <hexco.kp@gmail.com>
2016-11-02 00:21:52 -0400

    FIXED
    La porte a changer est en bas. Renovation bra.

--- TP1/Assets/Script/InventoryBackEnd.cs
+++ TP1/Assets/Script/InventoryBackEnd.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class InventoryBackEnd : MonoBehaviour {
-    public static float nbIce = 50;
+    public static float nbIce = 0;
     public static float nbCrystal = 0;
 	public static float nbWater = 0;
     public static float bagSize = 30;
 
     void Start()
     {
-        nbIce = 50;
+        nbIce = 0;
         nbCrystal = 0;
     }
 }
